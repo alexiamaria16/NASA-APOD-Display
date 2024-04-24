f179ba576efa3aa7ecd4dd27244bcecb0431d1e2
Visan Alexia Maria <125076704+alexiamaria16@users.noreply.github.com>
2024-04-24 16:19:07 +0300

    Add static HTML files

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 import requests
+
+#API KEY
+
+API_KEY='M0Zjmh7ytaX52ThAE1kFbhZlui1bOsUhb3Q24Iav'
+
+#API URL
+
+url='GET https://api.nasa.gov/planetary/apod'
+
 
 # Creează o instanță Flask
 app = Flask(__name__)
+freezer = Freezer(app)
 
 # Definirea unei rute pentru pagină principală
 @app.route("/")
 def index():
+
 
     response = requests.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 
@@ -21,6 +33,5 @@ def index():
     else:
         return 'Eroare la solicitarea API-ului NASA'
 
-# Rulează aplicația
 if __name__ == "__main__":
-    app.run(debug=True)
+    freezer.freeze()
